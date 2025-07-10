6cf1717caedac33a112a1501907d3ecc7e18cb06
GhostGamer6969 <arambhmani@hotmail.com>
2025-07-10 18:53:13 +0530

    initial commit

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("BFJAnDguu3KnUDCnCnacLLoXMtarWL6KoB919rNu9RK3");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
